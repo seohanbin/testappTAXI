8c0b7a003a5a346c23bf5dbc43a3bee9ba5fa007
seohanbin <scshb2000@naver.com>
2015-10-07 14:27:02 +0900

    index form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  #get 'home/index'
   root 'home#index'
-
+  match ':controller/:action/:id', via: [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
